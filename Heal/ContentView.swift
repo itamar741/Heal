0cf62d170ed5a3c286f865a87bc33e0770437025
Itamar <itamarshapira741@icloud.com>
2026-07-07 19:59:25 +0300

    Initial Commit

---
+++ Heal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Heal
+//
+//  Created by USER on 07/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Heal/HealApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealApp.swift
+//  Heal
+//
+//  Created by USER on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct HealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
